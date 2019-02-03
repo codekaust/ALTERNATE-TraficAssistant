8adc024154056717784d566cc00f048d0b3ee853
KaairaGupta <kgupta@es.iitr.ac.in>
2019-02-03 08:41:25 +0530

    single light adpp

---
+++ SingleLightApp/app/src/main/java/com/codaira/esummitui/TrafficFactors.kt
@@ -0,0 +1,3 @@
+package com.codaira.esummitui
+
+data class TrafficFactors (var controller:String="", var age:Int=0, var tieBreaker:Int=0, var cars:Int=0, var greenStartTime:Long= 0L, var greenForTime:Long=0L, var green : Boolean? = null)
